d83bb43813634acfe72f63d9cff7083de149af01
HackerHansen <58196022+HackerHansen@users.noreply.github.com>
2021-12-02 12:55:00 -0800

    Get rid of extra space
    I have no clue how that got there without me noticing but now it's bothering me so I'm getting rid of it

--- AmongUsCosmetics/AmongUsCosmetics.cpp
+++ AmongUsCosmetics/AmongUsCosmetics.cpp
@@ -18,7 +18,7 @@ DWORD GameAssemblySize;
 DWORD pid;
 HANDLE hprocess;
 
-HANDLE  hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
 
 map<char, char> byteDigits = { { '0', 0x0 }, { '1', 0x1 }, { '2', 0x2 }, { '3', 0x3 }, { '4', 0x4 }, { '5', 0x5 }, { '6', 0x6 }, { '7', 0x7 }, { '8', 0x8 }, { '9', 0x9 }, { 'A', 0xA }, { 'B', 0xB }, { 'C', 0xC }, { 'D', 0xD }, { 'E', 0xE }, { 'F', 0xF } };
 
