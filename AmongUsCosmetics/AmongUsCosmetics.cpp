5fa709a8d16c0f2d970b079dadf8ab00c5e6dd97
HackerHansen <58196022+HackerHansen@users.noreply.github.com>
2022-07-04 14:45:40 -0700

    Add comment thingy

--- AmongUsCosmetics/AmongUsCosmetics.cpp
+++ AmongUsCosmetics/AmongUsCosmetics.cpp
@@ -20,6 +20,7 @@ HANDLE hprocess;
 
 HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
 
+// I know I can just subtract but here we are. I might change this later tho 
 map<char, char> byteDigits = { { '0', 0x0 }, { '1', 0x1 }, { '2', 0x2 }, { '3', 0x3 }, { '4', 0x4 }, { '5', 0x5 }, { '6', 0x6 }, { '7', 0x7 }, { '8', 0x8 }, { '9', 0x9 }, { 'A', 0xA }, { 'B', 0xB }, { 'C', 0xC }, { 'D', 0xD }, { 'E', 0xE }, { 'F', 0xF } };
 
 bool isRunning(int procID) {
