2115f6f95dedaa8bfad76942c44a0325ccd4f1ef
HackerHansen <58196022+HackerHansen@users.noreply.github.com>
2022-10-23 17:43:25 -0700

    Thingy

--- AmongUsCosmetics/AmongUsCosmetics.cpp
+++ AmongUsCosmetics/AmongUsCosmetics.cpp
@@ -171,7 +171,7 @@ bool patchBytes(DWORD address, string bytes) {
 
 int main()
 {
-    SetConsoleTitleA("HackerHansen's Among Us Cosmetics Unlocker for v2022.10.25s");
+    SetConsoleTitleA("HackerHansen's Among Us Cosmetics Unlocker for v2022.10.25");
     cout << "Starting! \n";
     cout << "If you can afford to actually buy cosmetics, I would encourage doing so to support the devs who work hard on this game. \n";
 start:
